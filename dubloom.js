aa82ef2f77ac74cb484fff7d2a9290a175b388f0
Eric Sorenson <eric.sorenson@dubharmonic.com>
2011-06-25 16:34:22 -0500

    rearranging functions, added reset

--- dubloom.js
+++ dubloom.js
@@ -1,49 +1,39 @@
-window.addEventListener('load', eventWindowLoaded, false);    
-
-function eventWindowLoaded() {
-    dubloom();
-}
-
 function dubloom() {
 
+    // quit if canvas tag not supported
     if (!Modernizr.canvas) {
         return false;
     }
-
+    
+    // create references to the canvas
     var canvas = document.getElementById('canvas');
     var context = canvas.getContext('2d');
     var width = canvas.width;
     var height = canvas.height;  
 
-    // circle colors
-    var green = 'rgba(50,255,0, 1)';
-    var lightGreen = 'rgba(150,255,0, 1)';
-    var lighterGreen = 'rgba(200,255,0, 1)';
-    var yellow = 'rgba(255,255,0, 1)';
-    var orange = 'rgba(255,200,0, 1)';
-    var red = 'rgba(255,0,0, 1)';
-
-    // application states
-    const STATE_INIT = 0;
-    const STATE_WAIT = 1;
-    const STATE_WAIT_FOR_LOAD = 2;
-    const STATE_TITLE = 3;
-    const STATE_PLAY = 4;
-
-    // notes
-    const NOTE_1 = ["sine",0.0000,0.1000,0.0000,2.0000,1.0000,1.0000,20.0000,100.0000,2400.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,1.0000,0.0000,0.0000,0.0000,0.0000];
-    const NOTE_2 = ["sine",0.0000,0.1000,0.0000,2.0000,1.0000,1.0000,20.0000,200.0000,2400.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,1.0000,0.0000,0.0000,0.0000,0.0000];
-    const NOTE_3 = ["sine",0.0000,0.1000,0.0000,2.0000,1.0000,1.0000,20.0000,400.0000,2400.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,1.0000,0.0000,0.0000,0.0000,0.0000];
-    const NOTE_4 = ["sine",0.0000,0.1000,0.0000,2.0000,1.0000,1.0000,20.0000,600.0000,2400.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,1.0000,0.0000,0.0000,0.0000,0.0000];
-    const NOTE_5 = ["sine",0.0000,0.1000,0.0000,2.0000,1.0000,1.0000,20.0000,800.0000,2400.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,1.0000,0.0000,0.0000,0.0000,0.0000];
-    const NOTE_6 = ["sine",0.0000,0.1000,0.0000,2.0000,1.0000,1.0000,20.0000,1000.0000,2400.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,1.0000,0.0000,0.0000,0.0000,0.0000];
-
-
+    // background
     var backgroundGradient = context.createRadialGradient(width / 2, height / 2, 0, width / 2, height / 2, height / 1.1);
     backgroundGradient.addColorStop(0, '#444');
     backgroundGradient.addColorStop(1, '#000');
 
+    // circle collection
     var circles = [];
+
+    // circle colors
+    const green = 'rgba(50,255,0, 1)';
+    const lightGreen = 'rgba(150,255,0, 1)';
+    const lighterGreen = 'rgba(200,255,0, 1)';
+    const yellow = 'rgba(255,255,0, 1)';
+    const orange = 'rgba(255,200,0, 1)';
+    const red = 'rgba(255,0,0, 1)';
+
+    // notes
+    const note1 = ["sine",0.0000,0.1000,0.0000,2.0000,1.0000,1.0000,20.0000,100.0000,2400.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,1.0000,0.0000,0.0000,0.0000,0.0000];
+    const note2 = ["sine",0.0000,0.1000,0.0000,2.0000,1.0000,1.0000,20.0000,200.0000,2400.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,1.0000,0.0000,0.0000,0.0000,0.0000];
+    const note3 = ["sine",0.0000,0.1000,0.0000,2.0000,1.0000,1.0000,20.0000,400.0000,2400.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,1.0000,0.0000,0.0000,0.0000,0.0000];
+    const note4 = ["sine",0.0000,0.1000,0.0000,2.0000,1.0000,1.0000,20.0000,600.0000,2400.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,1.0000,0.0000,0.0000,0.0000,0.0000];
+    const note5 = ["sine",0.0000,0.1000,0.0000,2.0000,1.0000,1.0000,20.0000,800.0000,2400.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,1.0000,0.0000,0.0000,0.0000,0.0000];
+    const note6 = ["sine",0.0000,0.1000,0.0000,2.0000,1.0000,1.0000,20.0000,1000.0000,2400.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,1.0000,0.0000,0.0000,0.0000,0.0000];
 
     function play() {
         drawBackground();  
@@ -81,22 +71,22 @@ function dubloom() {
         circle.radius = 1;
         if(x < width/6) {
             circle.color = red;
-            circle.noteParams = NOTE_1;
+            circle.noteParams = note1;
         } else if(x < 2*width/6) {
             circle.color = orange;
-            circle.noteParams = NOTE_2;
+            circle.noteParams = note2;
         } else if(x < 3*width/6) {
             circle.color = yellow;
-            circle.noteParams = NOTE_3;
+            circle.noteParams = note3;
         } else if(x < 4*width/6) {
             circle.color = lighterGreen;
-            circle.noteParams = NOTE_4;
+            circle.noteParams = note4;
         } else if(x < 5*width/6) {
             circle.color = lightGreen;
-            circle.noteParams = NOTE_5;
+            circle.noteParams = note5;
         } else {
             circle.color = green;
-            circle.noteParams = NOTE_6;
+            circle.noteParams = note6;
         }
         circle.alpha = 1.0;
         
@@ -140,5 +130,11 @@ function dubloom() {
     const FRAME_RATE = 30;
     var intervalTime = 1000 / FRAME_RATE;
     setInterval( play, intervalTime );
+}
+
+window.addEventListener('load', eventWindowLoaded, false);    
+
+function eventWindowLoaded() {
+    dubloom();
 }
 
